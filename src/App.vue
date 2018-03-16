811b447abc3a2055ad08a422ad6f3059b1d7cc0e
Van Hoang <833design@gmail.com>
2018-03-17 00:17:44 +0700

    Optimized

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,8 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 html,body {
-  background-color: rgb(173, 173, 173);
+  background-color: #E4F1FE ;
 }
 </style>
